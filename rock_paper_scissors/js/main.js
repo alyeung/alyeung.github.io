e17333c0a044d113bef2952ff8669e7f27f63428
Allan Yeung <ayeung@revelconsulting.com>
2018-03-05 08:09:07 +0000

    minor improvements to rock paper scissors to include better comments, and allow the game to restart once we have reached best out of 3, included indentation

--- rock_paper_scissors/js/main.js
+++ rock_paper_scissors/js/main.js
@@ -6,37 +6,38 @@ var computerScore = 0;
 
 // Create a function play. It should accept two parameters: humanPlay and computerPlay.
 // Write all the code below INSIDE the function curly braces!!
-function play (humanPlay, computerPlay){
-  
+function play(humanPlay, computerPlay) {
+
   // Log "You played {humanPlay}. The bot played {computerPlay}" to the console.
   // For example: "You played rock. The bot played scissors."
-  console.log ("You played " + humanPlay+ ".  The bot played " + computerPlay + ".");
-  
+  console.log("You played " + humanPlay + ".  The bot played " + computerPlay + ".");
+
   //best 2 out of 3!!
-  if(computerScore < 3 && humanScore <3){
-  
+  if (computerScore < 3 && humanScore < 3) {
+
     // If the human play is equal to the computer play
-    if(humanPlay === computerPlay){
+    if (humanPlay === computerPlay) {
       // Log "You tied. :|" to the console.
       console.log("You tied. :|");
     }
     // Else if the humanPlay is 'paper' and the computerPlay is 'rock' OR
     // humanPlay is 'rock' and the computerPlay is 'scissors' OR
     // humanPlay is 'scissors' and the computerPlay is 'paper'
-      else if ( (humanPlay ==="paper" && computerPlay ==="rock") || 
-      (humanPlay ==="rock" && computerPlay ==="scissors") ||
-      (humanPlay ==="scissors" && computerPlay ==="paper")
-      ){
+    else if ((humanPlay === "paper" && computerPlay === "rock") ||
+      (humanPlay === "rock" && computerPlay === "scissors") ||
+      (humanPlay === "scissors" && computerPlay === "paper")
+    ) {
       // Log "You win! :)" to the console
       console.log("You win! :)");
       // Add one to the humanScore variable (be sure to use += )
-      humanScore +=1;
-    }else{
-  
-    // Otherwise log "You lose! :(" to the console
+      humanScore += 1;
+    }
+    else {
+
+      // Otherwise log "You lose! :(" to the console
       // Add one to the computerScore variable (be sure to use += )
       console.log("You lose! :(");
-      computerScore +=1;
+      computerScore += 1;
     }
     // Log "You have {humanScore} points" to the console
     // For example: "You have 0 points"
@@ -45,9 +46,13 @@ function play (humanPlay, computerPlay){
     console.log("You have " + humanScore + " points");
     console.log("The bot has " + computerScore + " points")
   }
-// END OF FUNCTION (Place the closing curly brace here!)
-  if(humanScore >2 || computerScore > 2){
+  // END OF FUNCTION (Place the closing curly brace here!)
+  if (humanScore > 2 || computerScore > 2) {
     console.log("Best 2 out of 3!  Game is done.");
+
+    // need to reset the score so it does not bomb
+    humanScore = 0;
+    computerScore = 0;
   }
 }
 
@@ -56,7 +61,9 @@ function play (humanPlay, computerPlay){
 // Hint: You'll need to use two arguments
 // each time you call the play function.
 
-var r= "rock"; var p ="paper"; var s = "scissors";
+var r = "rock";
+var p = "paper";
+var s = "scissors";
 
 //play(r, s);
 //play(s, s);
@@ -72,26 +79,30 @@ var r= "rock"; var p ="paper"; var s = "scissors";
 
 var roboMove;
 
-function computerMove(){
+// function to use Math.random to set the move for the robot, and return that value.
+function computerMove() {
   var computerChoice = Math.random();
-  if(computerChoice < 0.34){
+  if (computerChoice < 0.34) {
     roboMove = r;
   }
-  else if(computerChoice <=0.67){
+  else if (computerChoice <= 0.67) {
     roboMove = p;
   }
-  else{
+  else {
     roboMove = s;
   }
   console.log(computerChoice);
   return (roboMove);
 }
 
+// original function without random number
 // play(r, s);
 // play(s, r);
 // play(r, s);
 // play(s, r);
 // play(s, r);
+
+// play the game with the robot randomly going.  called 5+ times
 play(r, computerMove());
 play(s, computerMove());
 play(s, computerMove());
