2d0f7d389a67781ea8e5ff414762d3092f732229
Allan Yeung <ayeung@revelconsulting.com>
2018-03-07 08:45:45 +0000

    completed sidebar_menu exercise

--- 13_starter_interactions/sidebar_menu/js/main.js
+++ 13_starter_interactions/sidebar_menu/js/main.js
@@ -3,17 +3,18 @@
 // example: http://saraheholden.com/fewd53/classes/week_07/sidebar_menu/#
 
 // when click on hamburger 
-  $('hamburger').on('click', function(){
-    $('sidebar').toggleClass('sidebar')
-//  })
+  $('.hamburger').on('click', function(){
+    $('body').addClass('active');
+    $('.hamburger').hide();
+  })
 
     //bring out the side bar
     
 
 // when the side bar is open, click on the X
-//    $('.close').on('click', function(){
-        //$('.sidebar').toggleClass('sidebar');
-//        $('.sidebar')..removeClass('sidebar');
+    $('.close').on('click', function(){
+        $('body').removeClass('active');
+        $('.hamburger').show();
     })
     // add a class so the sidebar slides back to left
     // hamburger is shown
