359a847890844779fe0e53dea1ff40a582d6ea85
Allan Yeung <ayeung@revelconsulting.com>
2018-03-07 07:59:50 +0000

    class exercise modal javascript changes; still need to do the center of the modal

--- 13_starter_interactions/modal_window/js/main.js
+++ 13_starter_interactions/modal_window/js/main.js
@@ -1,1 +1,17 @@
+/* global $*/
 // Write your pseudo code here! HAPPY CODING!!
+
+// hide the modal
+$('.modal-background').hide();
+
+// when click for a good time
+$('#showModal').on('click', function(){
+    $('.modal-background').show();
+})
+    // modal shows up
+
+// when click on close
+$('#close').on('click', function(){
+    $('.modal-background').hide();
+})
+    // modal is hidden
