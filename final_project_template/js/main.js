ee4eaaa1b5e98608d37af418ad49c04817fa5744
Allan Yeung <ayeung@revelconsulting.com>
2018-03-13 00:27:49 +0000

    updated main.js to include pseudo code comments, added another topic for the bike/map.html page

--- final_project_template/js/main.js
+++ final_project_template/js/main.js
@@ -1,1 +1,59 @@
+///////////////
+// Feedback from Bryan: must have & nice, wish I have to have so we don't get stuck.
+// !!!!!!!!!!!!!!!!!(no hover on mobile devices ***** )
+//////////////
+
 // Make it rain!
+
+
+/////////////////
+// for map/bike.html page
+////////////////
+
+// Must Have: on mouse over of image
+    // place a partial opacity color box over the image choice/gear
+    // display white text heading and paragraph describing the gear and a link to Amazon.com
+    
+// Must Have: slide down vertically X pixels display blog text
+
+// Must Have: slide down vertically X pixels and slide from left to right the favorite links
+
+// Nice to Have: slide down more and display google map
+    // hover over icon for location name and description, with URL
+    // 3 bike shops: Recycled Cycles, Electric Lady, 2020 cycle 
+
+
+////////////////    
+// main page: portfolio
+/////////////////
+
+// when select sandwich bar 
+    // display side menu
+
+// when hover over the experience image, display complexity notes or stars?
+
+//////////////////////////
+//blog / feedback page:
+//////////////////////////
+
+// carousel effect for social media ways to contact:
+    // animate left or right on button click.
+    // display twitter image, github link image, linkedin
+
+// toggle down for filter on topics in blog
+    
+// fade in the feedback form
+
+
+///////////////////
+// dog page:
+///////////////////
+
+// when page loads
+    // animate bar like seen here: https://vebolife.com/
+    
+// show images of favorite products
+    // however over for title, and description
+    // click on item to shop
+    
+// reach bottom scale up pictures and change background color 
