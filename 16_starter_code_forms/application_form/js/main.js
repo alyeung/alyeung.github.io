1cae8f2d4cb91304107c8ffeec748ce3f678e7ce
Allan Yeung <ayeung@revelconsulting.com>
2018-03-15 05:05:52 +0000

    javascript for application form, error checking try

--- 16_starter_code_forms/application_form/js/main.js
+++ 16_starter_code_forms/application_form/js/main.js
@@ -2,6 +2,8 @@
 
 $('#submit').on('click', function() {
     event.preventDefault();
+    
+    
     // console log
     console.log("Console Log Stuff:");
     //console.log(course);
@@ -13,23 +15,46 @@ $('#submit').on('click', function() {
     
     var course = $("#course").val();
     
-    var city = $("#city").val();
+    
+    var city;
+    var sf= $("#sf").val();
+    var chi= $("#ch").val();
+    if(chi == 'undefined'){
+        city =sf;
+    }
+    else if (sf =='undefined'){
+        city=chi;
+    }
+    else{
+        city="I don't know"
+    }
     
     var past = $("#past").val();
+    var response = "not set";
+    if (past === "on"){
+        response = "yes"
+    }
+    else{
+        response = "no"
+    }
+    
+    // on or off
+    var updates = $("#updates").val();
     
     // need to handle checkbox
     
     // output
-    console.log(name);
+    console.log("Name is: " + name);
     
-    console.log(email);
+    console.log("Email is: " + email);
     
-    console.log(phone);
+    console.log("Phone Number is: " + phone);
     
-    console.log(course);
+    console.log("Course of interest is: " + course);
     
-    console.log(city);
+    console.log("City to take course: " + city);
     
-    console.log(past);
+    console.log("Past experience is as follows: " + past);
     
+    console.log("I would like to receive updates: " + response);
 })
