5d47b1144e1c151f7e31b77ee08dd0d84635b421
Allan Yeung <ayeung@revelconsulting.com>
2018-03-15 02:34:54 +0000

    application forms javascript console log out

---
+++ 16_starter_code_forms/application_form/js/main.js
@@ -0,0 +1,12 @@
+/* global $*/
+
+$('#submit').on('click', function() {
+    event.preventDefault();
+    // console log
+    console.log("Console Log Stuff:");
+    //console.log(course);
+    var name = $("#name").val();
+    
+ //   var email = ${"#"
+    console.log(name);
+})
