bc2bff954f11cb70ce5b112de43bbc57e1501d2b
Allan Yeung <ayeung@revelconsulting.com>
2018-03-20 01:54:35 +0000

    using  for lab

--- 17_starter_students_choice/this/js/main.js
+++ 17_starter_students_choice/this/js/main.js
@@ -1,10 +1,17 @@
-
+/* global $*/
 // -------------------------------------------------------------------------------
 // Part 1 - Click a box! Get them all!
 // -------------------------------------------------------------------------------
 
 // 1. When the user clicks an element with the class .box
   // fade that .box out (using the this keyword).
-
+$('.box').on('click', function(){
+  $(this).fadeOut(400);
+  
+});
 // 2. When the user clicks an element with the class .slide-me
   // slide that .slide-me up (using the this keyword).
+$('.slide-me').on('click', function(){
+  $(this).slideUp(400);
+  
+});
