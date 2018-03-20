12a522554973e5c2da09f0dbe98b5bef4b50b5c6
Allan Yeung <ayeung@revelconsulting.com>
2018-03-20 00:46:52 +0000

    added jquery error checking to application forms

--- 16_starter_code_forms/application_form/js/main.js
+++ 16_starter_code_forms/application_form/js/main.js
@@ -11,6 +11,10 @@ _EMAIL_ and _PHONE NUMBER_ fields. If they have not, under the associated field,
 tell the user what they need to do!
 */
 
+/* reference: http://madeline-omoore.com/FEWD_JAN/week_9/16_form_basics/solution/contact_form_solution_js/index.html */
+
+/*https://codepen.io/alyeung/pen/eMgvGQ */
+
 $('#submit').on('click', function() {
     event.preventDefault();
     
@@ -24,6 +28,8 @@ $('#submit').on('click', function() {
     var city;
     var sf= $("#sf").val();
     var chi= $("#ch").val();
+    console.log(chi, sf);
+    /*
     if(chi == 'undefined'){
         city =sf;
     }
@@ -33,11 +39,14 @@ $('#submit').on('click', function() {
     else{
         city="I don't know"
     }
+    */
 
     var past = $("#past").val();
     
     // on or off, handle the checkbox for updates
     var updates = $("#updates").val();
+
+
     var response = "not set";
     if (updates === "on"){
         response = "yes"
@@ -45,6 +54,77 @@ $('#submit').on('click', function() {
     else{ // checkbox was not selected
         response = "no"
     }
+    
+    /* handle name experience */
+    if(!name){
+        //alert('Please enter your name');
+        $('#name').addClass('warn');
+        $('#name-error-message').fadeIn(300);
+    } else{
+        $('#name').removeClass('warn');
+        //alert('Submitted!');
+        $('#name-error-message').fadeOut(300);
+    }
+    /* handle email experience */    
+    if(!email ){
+        //alert('Please enter your email');
+        $('#email').addClass('warn');
+        $('#email-error-message').fadeIn(300);
+    } else{
+        $('#email').removeClass('warn');
+        //alert('Submitted!');
+        $('#email-error-message').fadeOut(300);
+    }
+    
+    /* handle phone experience */
+    if(!phone ){
+        //alert('Please enter your email');
+        $('#phone').addClass('warn');
+        $('#phone-error-message').fadeIn(300);
+    } else{
+        $('#phone').removeClass('warn');
+        //alert('Submitted!');
+        $('#phone-error-message').fadeOut(300);
+    }
+ 
+     /* handle course interest  */
+    if(!course ){
+        //alert('Please enter your email');
+        $('#course').addClass('warn');
+        $('#course-error-message').fadeIn(300);
+    } else{
+        $('#course').removeClass('warn');
+        //alert('Submitted!');
+        $('#course-error-message').fadeOut(300);
+    }
+
+    /* handle city of interest  */
+    /* .isChecked */
+    if($('#sf').is(':checked') ||  $('#ch').is(':checked')){
+        $('#sf').removeClass('warn');
+        $('#ch').removeClass('warn');
+        //alert('Submitted!');
+        $('#city-error-message').fadeOut(300);
+    } else{
+        //alert('Please enter your email');
+        $('#sf').addClass('warn');
+        $('#ch').addClass('warn');
+        $('#city-error-message').fadeIn(300);
+    }
+    
+    
+    /* handle past experience */
+    if(!past ){
+        //alert('Please enter your email');
+        $('#past').addClass('warn');
+        $('#past-error-message').fadeIn(300);
+    } else{
+        $('#past').removeClass('warn');
+        //alert('Submitted!');
+        $('#past-error-message').fadeOut(300);
+    }
+    
+    
     
     // if not null
     if(name){
