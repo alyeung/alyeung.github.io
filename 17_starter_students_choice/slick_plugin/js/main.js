d9d392a9dfe8ae3a2c9f7233fdd8a25f9bc8dd47
Allan Yeung <ayeung@revelconsulting.com>
2018-03-20 01:05:18 +0000

    added 17 jquery plugin exercise

---
+++ 17_starter_students_choice/slick_plugin/js/main.js
@@ -0,0 +1,7 @@
+/* global $*/
+
+// STEP 4: Initialize Slick! Choose an option here: http://kenwheeler.github.io/slick/
+// Make sure to replace the slick selector with the class you have on the carousel
+// Wrapper in your HTML (in our example .carousel-wrapper)
+
+$('.carousel-wrapper').slick();
---
+++ 17_starter_students_choice/this/js/main.js
@@ -0,0 +1,10 @@
+
+// -------------------------------------------------------------------------------
+// Part 1 - Click a box! Get them all!
+// -------------------------------------------------------------------------------
+
+// 1. When the user clicks an element with the class .box
+  // fade that .box out (using the this keyword).
+
+// 2. When the user clicks an element with the class .slide-me
+  // slide that .slide-me up (using the this keyword).
