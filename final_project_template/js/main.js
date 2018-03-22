f8578285b51a1b2700cb27159452e04de11b8d6c
Allan Yeung <ayeung@revelconsulting.com>
2018-03-22 00:36:58 +0000

    added fixed menu for index.html page

--- final_project_template/js/main.js
+++ final_project_template/js/main.js
@@ -87,7 +87,23 @@ $('#postmanCloseButton').on('click', function(){
     $('#postmanModal').hide();
 });
 
+// smooth scroll
+// Do it when someone clicks a nav link
+$('navigation a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('html, body').animate({
+    scrollTop: targetOffset
+  }, 600);
 
+});
 
 // handle feedback form
 
